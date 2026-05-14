a3142f4c5e5c921346c07f71366f2f5cf83e03f5
Kaif <shaikhkaif96840@gmail.com>
2026-05-14 16:07:51 +0530

    hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { WordsPullUp } from './WordsPullup'
-
 
 const HeroSection = () => {
   return (
@@ -15,7 +13,7 @@ const HeroSection = () => {
         }}>
       </div>
       <div className='relative h-[60vh] flex justify-center lg:px-28 items-center flex-col md:gap-8 gap-6 lg:gap-12'>
-      <WordsPullUp text='Build Your Strongest Self' className='font-bold font-oswald lg:text-7xl md:text-5xl text-3xl text-primary z-10' />
+      <h1 className='font-bold font-oswald lg:mx-28 lg:text-7xl md:text-5xl text-3xl text-primary z-10'>Build Your Strongest Self</h1>
       <span className='text-primary font-figtree lg:py-0 w-[90vw] text-center lg:text-left lg:w-[60vw]'>Galaxy Fitness is built for people who want real results — from strength training and fat loss to endurance and athletic performance. Modern equipment, expert trainers, and an atmosphere designed to keep you motivated every day.</span>
       <button className='text-secondary font-bold font-figtree text-2xl px-4 py-2 rounded-2xl cursor-pointer hover:scale-110 transition-all duration-300 bg-primary active:scale-90'>Join Galaxy Fitness</button>
       </div>
